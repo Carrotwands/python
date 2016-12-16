1ad4fefbebe7a93c072f3fade5aa13b3594a6547
Carrotwands <carrotwands@gmail.com>
2016-12-16 21:00:10 +0100

    Update combined.py

--- combined.py
+++ combined.py
@@ -4,26 +4,11 @@ from progressbar import ProgressBar, Percentage, Bar
 def dlProgress(count, blockSize, totalSize):
     pbar.update( int(count * blockSize * 100 / totalSize) )
 
-#url = raw_input("Irj be egy URL-t!")
-#fileName = raw_input("Mi legyen a neve?")+".mp4"
-#pbar = ProgressBar(widgets=[Percentage(), Bar()])
-#urlretrieve(url, fileName, reporthook=dlProgress)
-
-
-
-
-
-
-#--------------------------------
 n=0
 
 file = open('test.txt', 'r')
 text = file.read()
 lines = text.split('\n')
-
-
-
-#print 'TITLE: '+title+' URL: '+url
 
 while (n < len(lines)):
        
@@ -37,8 +22,3 @@ while (n < len(lines)):
 
        n=n+1
        print fileName+' download finished'
-
-
-
-       
-
